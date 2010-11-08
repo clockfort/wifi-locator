4d540978980be37f569f339450f24a33e0ce4172
Chris Lockfort <clockfort@gmail.com>
2010-11-08 12:39:34 -0500

    more test data points, map of test point coverage, made xml to hash function

--- input-interpreter.pl
+++ input-interpreter.pl
@@ -15,21 +15,29 @@ my $data = $xml->parse_fh($XML_INPUT);
 my @cells;
 
 #Populate array of cells
-#current xml keys: 'frequency', 'signal', 'address', 'quality', 'essid', 'noise'
 foreach my $cell (@{$data->{cell}}){
-	my %current_cell = ();
-	$current_cell{'frequency'} = $cell->{frequency};
-	$current_cell{'signal'} = $cell->{signal};
-	$current_cell{'signal'} =~ s/ dBm//;
-	$current_cell{'address'} = $cell->{address};
-	$current_cell{'quality'} = $cell->{quality};
-	$current_cell{'essid'} = $cell->{essid};
-	$current_cell{'noise'} = $cell->{noise};
+	my %current_cell = xml_cell_to_hash_cell($cell);
 	push(@cells,\%current_cell);
 }
 
-my @sorted_cells = sort { ${$a}{'signal'} <=> ${$b}{'signal'} } @cells;
 
+#Testing sorting
+my @sorted_cells = sort { ${$a}{'signal'} <=> ${$b}{'signal'} } @cells;
 foreach my $cell (@sorted_cells){
 	print ${$cell}{'signal'}, "\n";
+}
+
+
+#current xml keys: 'frequency', 'signal', 'address', 'quality', 'essid', 'noise'
+sub xml_cell_to_hash_cell {	
+	my $cell = shift;
+        my %current_cell = ();
+        $current_cell{'frequency'} = $cell->{frequency};
+        $current_cell{'signal'} = $cell->{signal};
+        $current_cell{'signal'} =~ s/ dBm//;
+        $current_cell{'address'} = $cell->{address};
+        $current_cell{'quality'} = $cell->{quality};
+        $current_cell{'essid'} = $cell->{essid};
+        $current_cell{'noise'} = $cell->{noise};
+	return %current_cell;
 }
